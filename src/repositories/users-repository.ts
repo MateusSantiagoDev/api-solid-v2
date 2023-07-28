cb8ad8ff449d61562179a0e8de11a56feb5aec38
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-07-28 18:59:37 -0300

    feat: create of user agreement

---
+++ src/repositories/users-repository.ts
@@ -0,0 +1,10 @@
+import { Prisma, User } from '@prisma/client'
+
+export interface UsersRepository {
+
+  findByEmail(email: string): Promise<User>
+
+  create(data: Prisma.UserCreateInput): Promise<User>
+  
+}
+
