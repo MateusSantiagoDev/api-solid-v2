41cb361518c16c204c5ff79c3b7fae066c81afa6
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-07-28 16:25:53 -0300

    feat: add server configuration

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
+import { app } from './app'
+import { env } from './env-config'
 
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🚀 HTTP Server Running!')
+  })
