7df6d1685e966a8c1eba55b3b769bddef05824f9
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-08-01 20:26:30 -0300

    feat: create SearchGymsUseCase method

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -27,4 +27,10 @@ export class InMemoryGymsRepository implements GymsRepository {
     this.items.push(gym)
     return gym
   }
+
+  async searchMany(query: string, page: number) {
+    return this.items
+      .filter((item) => item.title.includes(query))
+      .slice((page - 1) * 20, page * 20) //começa do zero e cada pagina tem 20
+  }
 }
---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,23 @@
+import { Gyn } from '@prisma/client'
+import { GymsRepository } from '@/repositories/gyms-repository'
+
+interface SearchGymsUseCaseRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gyn[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page)
+    return { gyms }
+  }
+}
