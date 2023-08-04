87a0060c5d3d4e49492b4153771e5130a42dbe2e
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-08-04 20:03:55 -0300

    feat: add makeGetUserProfileUseCase in profile route

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,23 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   // buscando o token no headers e validando ele
   await request.jwtVerify()
 
-  return reply.status(200).send()
+  // instanciando a factory de perfil
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  // buscando o usuário pelo id que veio no headers
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  // retorno p usuário sem o password
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
