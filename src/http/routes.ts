a6a20b483b4df3d7f2ed7a6bc346b40bb4f2e357
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-08-04 21:41:26 -0300

    feat: update in project

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)  
   app.post('/sessions', authenticate)
 
   // rotas que serão chamadas somente se o usuário estiver autenticado
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
